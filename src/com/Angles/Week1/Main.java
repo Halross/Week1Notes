e367f5e40268d789b5bf8c4548e184204e81cd6f
12th GCHS Connor A Angles <018120@SWCS.K12>
2019-01-17 12:46:37 -0500

    Initial commit

---
+++ src/com/Angles/Week1/Main.java
@@ -0,0 +1,17 @@
+package com.Angles.Week1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+
+        double temperature;
+        int zipcode;
+        String city;
+        String humidity;
+
+        int dailyHighs[] = {1,2,3};
+        System.out.println(dailyHighs[0]);
+    }
+}
